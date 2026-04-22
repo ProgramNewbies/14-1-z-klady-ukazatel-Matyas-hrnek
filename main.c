9a8075b2fbf2acb9ba816d0f096e82a5eed21108
Matyas-hrnek <matyas.hanek@student.spsmb.cz>
2026-04-22 15:19:58 +0200

    Add files via upload

---
+++ main.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+
+int main() {
+
+int a = 0;
+int b = 0;
+int c = 0;
+int max = 0;
+
+int *p_a = &a;
+int *p_b = &b;
+int *p_c = &c;
+
+printf("zadej prvni cislo: ");
+scanf("%d", &a);
+printf("zadej druhe cislo: ");
+scanf("%d", &b);
+printf("zadej treti cislo: ");
+scanf("%d", &c);
+
+if(*p_a > max) {
+    max = *p_a;
+}
+
+if(*p_b > max) {
+    max = *p_b;
+}
+
+if(*p_c > max) {
+    max = *p_c;
+}
+
+printf("Soucet: %d\n",*p_a + *p_b + *p_c);
+printf("Maximum: %d\n", max);
+
+
+if((*p_a + *p_b + *p_c )% 2 == 0) {
+    printf("Soucet je sudy");
+
+}
+else{
+    printf("Soucet je lichy");
+}
+
+    
+}
